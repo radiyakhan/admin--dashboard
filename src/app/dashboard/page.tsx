ce458dbf70d7619e377ae9ed169a6af339473372
radiyakhan <168372745+radiyakhan@users.noreply.github.com>
2025-02-09 00:20:24 +0500

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -66,7 +66,7 @@ const Dashboard = () => {
           </h3>
           {products.length > 0 ? (
             <ul className="space-y-4">
-              {products.map((product) => (
+              {products.map((product : any) => (
                 <li key={product._id} className="flex justify-between items-center border-b pb-2">
                   <div className="flex items-center gap-4">
                     <img
